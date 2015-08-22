328d497d011991f08cd927993e3163ed2ee8bfcf
Uldzh <uldis.megnis@gmail.com>
2015-08-22 09:08:12 +0300

    Add api base controller

---
+++ app/controllers/api/v1/base_controller.rb
@@ -0,0 +1,38 @@
+module Api
+  module V1
+    class BaseController < ApplicationController
+      protect_from_forgery with: :null_session
+
+      respond_to :json
+
+      private
+      def render_401
+        render nothing: true, status: 401
+      end
+
+      def render_403
+        render nothing: true, status: 403
+      end
+
+      def render_404
+        render nothing: true, status: 404
+      end
+
+      def publish( redis_channel, message )
+        Redis.new.publish redis_channel, message
+      end
+
+      def user?
+        current_user.try(:permission_group) == User::PERMISSION_USER
+      end
+
+      def admin?
+        current_user.try(:permission_group) == User::PERMISSION_ADMIN
+      end
+
+      def guest?
+        current_user.try(:permission_group) == User::PERMISSION_GUEST
+      end
+    end
+  end
+end
