14f0d1939b9c35ea7be7b91e228cf70f20b8f3c2
Uldzh <uldis.megnis@gmail.com>
2015-08-22 09:55:50 +0300

    Fix missing api base_controller for 403 case

--- spec/controllers/api/v1/base_controller_spec.rb
+++ spec/controllers/api/v1/base_controller_spec.rb
@@ -20,12 +20,12 @@ describe Api::V1::BaseController do
   end
 
   describe '#render_403' do
-    it 'returns empty body with 401 status code' do
-      routes.draw { get 'render_401' => 'api/v1/base#render_401' }
-      get :render_401
+    it 'returns empty body with 403 status code' do
+      routes.draw { get 'render_403' => 'api/v1/base#render_403' }
+      get :render_403
 
       expect(response.body).to eq('')
-      expect(response.status).to eq(401)
+      expect(response.status).to eq(403)
     end
   end
 
@@ -40,4 +40,10 @@ describe Api::V1::BaseController do
   end
 
 
+  describe "#publish" do
+    it "publishes given message to given redis channel" do
+      expect(Redis).to receive_message_chain('new.publish').with('chan', 'Test message')
+      controller.send(:publish, 'chan', 'Test message')
+    end
+  end
 end
