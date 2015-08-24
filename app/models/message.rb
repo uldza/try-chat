bde1cbcc27b24eaade1b55458a5a3686923c8a7d
Uldzh <uldis.megnis@gmail.com>
2015-08-24 06:32:27 +0300

    Add channel scope for message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ActiveRecord::Base
   belongs_to :channel
 
   validates_presence_of :text, :user, :channel
+
+  scope :for_channel, ->(chan_id) { where(channel_id: chan_id) }
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 describe Message do
+  let(:user) { create(:user) }
+  let(:message) { create(:message, user: user) }
+  let(:message_2) { create(:message, user: user) }
+  let(:message_3) { create(:message, user: user, channel: message_2.channel) }
+
   it_behaves_like "association includer", :belongs_to, :user
   it_behaves_like "association includer", :belongs_to, :channel
 
@@ -12,5 +17,18 @@ describe Message do
     it { expect(subject.errors[:text].size).to eq(1) }
     it { expect(subject.errors[:user].size).to eq(1) }
     it { expect(subject.errors[:channel].size).to eq(1) }
+  end
+
+  describe ".scopes" do
+    describe ".for_channel" do
+      it "returns messages for given channel" do
+        message
+        message_2
+        message_3
+        resources = Message.for_channel( message_2 )
+        expect( resources ).not_to include( message )
+        expect( resources ).to include( message_2, message_3 )
+      end
+    end
   end
 end
