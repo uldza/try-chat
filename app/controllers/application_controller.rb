13a4257b47a59dab0415340efc3b76a9d295657a
Uldzh <uldis.megnis@gmail.com>
2015-08-25 14:06:23 +0300

    Handle routing, server, record_not_found errors for application, when not in dev

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,27 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  rescue_from(Exception) { |ex| handle_error(ex) } unless ENV['RAILS_ENV'] == 'development'
+
   before_action :authenticate_user!
 
   def index
+  end
+
+  def handle_error(exception = nil)
+    @status = 500
+
+    case exception
+    # exception is nil when it happened in a middleware (routing errors)
+    when ActionController::RoutingError, ActiveRecord::RecordNotFound, nil
+      @status = 404
+    else
+      # Ussualy here I add to notify some external monitoring solution
+      # env["airbrake.error_id"] = notify_airbrake(exception)
+    end
+
+    @url = '/'
+
+    render :handle_error, layout: false, status: @status
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'application#index'
 
   get '/socket' => 'sockets#message'
+
+  namespace :api do
+    namespace :v1 do
+      resources :messages
+      resources :channels
+    end
+  end
+
+  root to: 'application#index'
+
+  match "*not_found_path", to: "application#handle_error", via: [:get, :post], format: false
 end
